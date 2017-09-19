14449ad6bd7dd1a5444b5c25895e9004b13cfd3c
Auxane <delinguadesain@et.esiea.fr>
2017-09-19 18:10:33 +0200

    auth service implemented : to debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
-import { Connection } from './connection';
 import { AuthService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['../assets/css/materialize.css', '../assets/css/main.css'],
   providers: [AuthService]
 })
 export class AppComponent {
   login = '';
   pass = '';
-  log: Connection ;
-  constructor(private authService: AuthService) { }
+  connected = '';
+
+  constructor(private authService: AuthService) {
+  }
+
+  onConnect(): any {
+    this.connected = this.login ;
+    if (this.authService.auth(this.login, this.pass) === 1) {
+      this.connected = 'connected';
+    }
+    if (this.authService.auth(this.login, this.pass) === 0) {
+      this.connected = 'wrong credential';
+    }
+  }
 }
-
-
 
--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -4,9 +4,10 @@ import { Connection } from './connection';
 @Injectable()
 export class AuthService {
   user: Connection;
-  Auth(login: string, pass: string): any {
+  auth(login: string, pass: string): any {
     if (this.user.auxane.email === login && this.user.auxane.password === pass) {
      return 1;
     }
+    return 0;
   }
 }
