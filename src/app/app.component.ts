1e8f163fd899410b4baf6e32f76c787e69d87e34
Hamashi <hamashii@gmail.com>
2017-09-19 14:19:10 +0200

    Service Auth + fake User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Connection } from './connection';
+import { AuthService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../assets/css/materialize.css', '../assets/css/main.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
-
 export class AppComponent {
-  title = 'Login';
-
-
+  login = '';
+  pass = '';
+  log: Connection ;
+  constructor(private authService: AuthService) { }
 }
+
+
+
