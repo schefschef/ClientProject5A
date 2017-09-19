25ddd88c1aac31fe6100e5cc57b1479daf218937
schefschef <scheffer@et.esiea.fr>
2017-09-19 14:04:31 +0200

    login view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/css/materialize.css', '../assets/css/main.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Login';
+
+
 }
